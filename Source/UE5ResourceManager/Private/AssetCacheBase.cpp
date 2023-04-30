e64b54183ecce379dfc27183f1308c5f5ee417c3
Kay Priesnitz <kpriesnitz@outlook.de>
2023-04-30 12:14:41 +0200

    ADD AssetCache Interface

---
+++ Source/UE5ResourceManager/Private/AssetCacheBase.cpp
@@ -0,0 +1,1 @@
+﻿#include "AssetCacheBase.h"
---
+++ Source/UE5ResourceManager/Public/AssetCacheBase.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include <mutex>
+
+class AssetCacheBase
+{
+public:
+	virtual ~AssetCacheBase() = default;
+
+	virtual void UnloadAsset(FString assetPath) = 0;
+
+	virtual void UpdateAssetPriority(FString assetPath, int newPriority) = 0;
+
+	virtual void EvictAsset() = 0;
+
+protected:
+	std::mutex m_cacheMutex;
+};
