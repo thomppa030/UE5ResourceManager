24d27af78704903390a0c6bbd26847a4f9d6b740
Kay Priesnitz <kpriesnitz@outlook.de>
2023-04-30 12:14:02 +0200

    ADD ResourceManager Class

---
+++ Source/UE5ResourceManager/Private/ResourceManager.cpp
@@ -0,0 +1,56 @@
+﻿#include "ResourceManager.h"
+
+#include "AssetCache.h"
+
+ResourceManager::ResourceManager(size_t maxMemorySize)
+	: m_MaxMemory(maxMemorySize)
+{
+}
+
+template <typename AssetType>
+TFuture<TSharedPtr<AssetType>> ResourceManager::LoadAssetAsync(const FString& assetPath, int priority)
+{
+	static_assert(TIsDerivedFrom<AssetType, UObject>::IsDerived, "AssetType must be derived from UObject");
+	// Lock the cache mutex so we can safely access the cache
+	m_cacheMutex.Lock();
+
+	// Get the asset class from the asset type
+	const TSubclassOf<UObject> AssetClass = AssetType::StaticClass();
+	if (!m_AssetCaches.Contains(AssetClass))
+	{
+		m_AssetCaches.Add(AssetClass, MakeUnique<AssetCache<AssetType>>(m_MaxMemory));
+	}
+
+	TUniquePtr<AssetCacheBase>& Cache = m_AssetCaches[AssetClass];
+	AssetCache<AssetType>* TypedCache = static_cast<AssetCache<AssetType>*>(Cache.Get());
+
+	m_cacheMutex.Unlock();
+
+	TFuture<TSharedPtr<AssetType>> AssetFuture = TypedCache->LoadAsset(assetPath);
+	m_PriorityQueue.Insert(assetPath, priority);
+	
+	return AssetFuture;
+}
+
+template <typename AssetType>
+void ResourceManager::UnloadAsset(const FString& assetPath)
+{
+	static_assert(TIsDerivedFrom<AssetType, UObject>::IsDerived, "AssetType must be derived from UObject");
+
+	m_cacheMutex.Lock();
+
+	TSubclassOf<UObject> AssetClass = AssetType::StaticClass();
+
+	if (m_AssetCaches.Contains(AssetClass))
+	{
+		TUniquePtr<AssetCacheBase>& Cache = m_AssetCaches[AssetClass];
+		Cache->UnloadAsset(assetPath);
+		m_PriorityQueue.Remove(assetPath);
+	}
+
+	m_cacheMutex.Unlock();
+}
+
+void ResourceManager::Tick(float DeltaTime)
+{
+}
---
+++ Source/UE5ResourceManager/Public/ResourceManager.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "PriorityQueue.h"
+
+class AssetCacheBase;
+
+class ResourceManager
+{
+public:
+	ResourceManager(size_t maxMemorySize);
+
+	template<typename AssetType>
+	UE_NODISCARD TFuture<TSharedPtr<AssetType>> LoadAssetAsync(const FString& assetPath, int priority = 0);
+
+	template<typename AssetType>
+	void UnloadAsset(const FString& assetPath);
+
+	void Tick(float DeltaTime);
+
+private:
+
+	size_t m_MaxMemory;
+
+	TMap<TSubclassOf<UObject>, TUniquePtr<AssetCacheBase>> m_AssetCaches;
+
+	FPriorityQueue m_PriorityQueue;
+
+	FCriticalSection m_cacheMutex;
+};
+
